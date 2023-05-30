eb085f0375045d25e0f2f284f70eba37dc4cee69
Lucius Holzinger <luciholzinger@gmail.com>
2023-05-29 17:00:53 -0700

    Now processed the data according to the boudnaries. I am confused about question 2 though

--- ProcessGameState.py
+++ ProcessGameState.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import json
 
 
 class ProcessGameState:
@@ -8,13 +7,39 @@ class ProcessGameState:
         self.weapon_classes = None
         self.game_state = pd.read_parquet('data/game_state_frame_data.parquet', engine='pyarrow')
         self.inventory_class = pd.DataFrame()
+        self.coord_processed = pd.DataFrame()
+
+
+    def is_within_quadrilateral(self, point, A, B, C, D):
+        x, y = point
+
+        # Calculate the equations of the sides
+        eq_AB = lambda x, y: (B[1] - A[1]) * (x - A[0]) - (B[0] - A[0]) * (y - A[1])
+        eq_BC = lambda x, y: (C[1] - B[1]) * (x - B[0]) - (C[0] - B[0]) * (y - B[1])
+        eq_CD = lambda x, y: (D[1] - C[1]) * (x - C[0]) - (D[0] - C[0]) * (y - C[1])
+        eq_DA = lambda x, y: (A[1] - D[1]) * (x - D[0]) - (A[0] - D[0]) * (y - D[1])
+
+        # Check if the point satisfies the inequalities for all sides
+        if eq_AB(x, y) >= 0 and eq_BC(x, y) >= 0 and eq_CD(x, y) >= 0 and eq_DA(x, y) >= 0:
+            return True
+        else:
+            return False
 
 
     def process_coord_boundary(self):
         self.game_state = self.game_state.loc[(self.game_state['z'] >= 285) & (self.game_state['z'] <= 421)]
-        self.game_state = self.game_state.loc[(self.game_state['y'] >= 250) & (self.game_state['y'] <= 1233)]
-        self.game_state = self.game_state.loc[(self.game_state['x'] >= -2806) & (self.game_state['x'] <= -1565)]
-        self.game_state.reset_index(inplace=True)
+        # self.game_state.reset_index(inplace=True)
+        for index, row in self.game_state.iterrows():
+            A = (-2806, 742)
+            B = (-2472, 1233)
+            C = (-1565, 580)
+            D = (-1735, 250)
+            point = row[['x', 'y']]
+
+            if self.is_within_quadrilateral(point, A, B, C, D):
+                self.coord_processed = self.coord_processed._append(row)
+
+            print(row[['x', 'y']])
 
 
     def process_inventory_classes(self):
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+"""
+Important resources:
+https://pandas.pydata.org/docs/index.html
+https://numpy.org/doc/stable/index.html
+"""
 from ProcessGameState import ProcessGameState
 
 
 def main():
     pgs = ProcessGameState()
     pgs.process_coord_boundary()
-    pgs.process_inventory_classes()
     print("this is awesome")
+    pgs.process_inventory_classes()
 
 
 if __name__ == '__main__':
